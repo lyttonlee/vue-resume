37fc3019b2cf64aa8c83e28e97070f6f5323a19e
lyttonlee <lzr3278@163.com>
2018-10-25 16:23:51 +0800

    resume

--- src/views/info.vue
+++ src/views/info.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="page-item section">
-    <h2>info</h2>
-    <p>dskjladhsajkdh</p>
+    <h2>基本信息</h2>
+    <mu-avatar size=80>
+      <img src="../assets/logo.png">
+    </mu-avatar>
+    <h3>博学之,审问之,慎思之,明辨之,笃行之</h3>
     <div>
-      <p>daksjdhaksh</p>
+      <mu-paper class="paper" circle :z-depth="4">
+        <i class="iconfont icon-dian"></i>
+      </mu-paper>
+      <mu-paper class="paper" circle :z-depth="4">
+        <i class="iconfont icon-dian"></i>
+      </mu-paper>
     </div>
   </div>
 </template>
 <style lang="less" scoped>
 .section {
   background-color: rgb(20, 73, 73) !important;
+  .paper {
+    width: 50px;
+    height: 50px;
+    background-color: rgba(16, 68, 180, 0.363);
+  }
 }
 </style>
